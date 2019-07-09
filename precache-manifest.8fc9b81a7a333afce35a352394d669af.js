4496ab36147b45d8bc647238ec06c2d4fce879a3
Scott Savarie <scottsavarie@gmail.com>
2019-07-09 08:38:06 +0200

    Updates

---
+++ precache-manifest.8fc9b81a7a333afce35a352394d669af.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eeb8114dcdf5118beae345eaabb5f79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aad6f8137f66759ccd6",
+    "url": "/static/css/main.109eeed9.chunk.css"
+  },
+  {
+    "revision": "bf939ec5a5a33e209433",
+    "url": "/static/js/2.08f4d5dd.chunk.js"
+  },
+  {
+    "revision": "8aad6f8137f66759ccd6",
+    "url": "/static/js/main.f8472d60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c60adaaabf94c786a8689f0e9c8d8154",
+    "url": "/static/media/bandcamp.c60adaaa.svg"
+  },
+  {
+    "revision": "65f8f19034abf7c2ff2229cd46767804",
+    "url": "/static/media/bg.65f8f190.jpg"
+  },
+  {
+    "revision": "4a6dea0cafe3a67f6cb1053624a958b2",
+    "url": "/static/media/close.4a6dea0c.svg"
+  },
+  {
+    "revision": "7043267174e89189676f601039bd07cd",
+    "url": "/static/media/fb.70432671.svg"
+  },
+  {
+    "revision": "d667e876d9dc146a65cfc2cc6eec8f19",
+    "url": "/static/media/instagram.d667e876.svg"
+  },
+  {
+    "revision": "3370468873f08888ae6c1d3f4a454e12",
+    "url": "/static/media/play.33704688.svg"
+  },
+  {
+    "revision": "a3e78dd5f11aca7a80d1c828b2d0d5c3",
+    "url": "/static/media/spotify.a3e78dd5.svg"
+  },
+  {
+    "revision": "89eaa7422b26701cdfabbdf4fde0e5ee",
+    "url": "/static/media/twitter.89eaa742.svg"
+  },
+  {
+    "revision": "31700ff45894b5f63495c6c4fa27e3d4",
+    "url": "/static/media/youtube.31700ff4.svg"
+  }
+]);
